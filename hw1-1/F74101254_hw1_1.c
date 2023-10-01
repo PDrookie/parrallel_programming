ee5f515b784139202be3d753cd43d3f14bf5a680
JimC <91251339+PDrookie@users.noreply.github.com>
2023-10-01 14:01:56 +0800

    Add files via upload

--- hw1-1/F74101254_hw1_1.c
+++ hw1-1/F74101254_hw1_1.c
@@ -1,47 +1,57 @@
 #include<stdio.h>
+#include<math.h>
 #include<stdlib.h>
-//#include<mpi.h>
+#include<mpi.h>
 
 int main(int argc, char *argv[]){
-    char file_name[50];
-    scanf("%s", file_name);
-    FILE *file = fopen(file_name, "r");
-    if(file = NULL){
-        pritnf("Could not open the file %s\n", file_name);
-        return 1;
-    }
-    int my_rank, comm_sz;
-    int source;
+    int n, m;
+    unsigned int tests[32] = {0}, covered = 0;
+    int process_rank, global_sz;
+    int total_combination = 0, local_combination = 0;
 
-    MPI_Init(NULL, NULL);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-
-    int n, m, k;
-    int total_combination, local_combination;
-    int part[32][32];
-    int cost[32];
-    fscanf(file, "%d %d", &n, &m);
-    for(int i = 0; i < m; i ++){
-        fscanf(file, "%d %d\n", &k, &cost[i]);
-        for(int j = 0; j < k; j ++){
-            fscanf(file, "%d", &part[i][j]);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &global_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+    
+    if(process_rank == 0){
+        char file_name[128];
+        scanf("%s", file_name);
+        FILE *file = fopen(file_name, "r");
+        fscanf(file, "%d %d", &n, &m);
+        for(int i = 0; i < m; i ++){
+            int part_size, part, cost[32];
+            fscanf(file, "%d %d\n", &part_size, &cost[i]);
+            for(int j = 0; j < part_size; j ++){
+                fscanf(file, "%d", &part);
+                tests[i] |= 1 << (part - 1);
+            }
         }
+        fclose(file);
     }
+    
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&tests, m, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
 
-    int test = (int*)malloc(sizeof(int) * n);
-
-    if(my_rank != 0){
-        MPI_send();
-    }
-    else{
-        total_combination = local_combination;
-        for(int i = 1; i <  ;i ++){
-            MPI_recv();
-            total_combination += local_combination;
+    for(int i = process_rank; i < pow(2, m - 1); i += global_sz){
+        for(int j = 32; j >= 0; j --){
+            if(i & j){
+                covered |= tests[i];
+            }
         }
+        if(covered == (1 << n) - 1){
+            local_combination ++;
+        }
+
     }
+
+    MPI_Reduce(&local_combination, &total_combination, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Finalize();
+
+    if(process_rank == 0){
+        printf("%d", total_combination);
+    }
+
     return 0;
 
 }
